55f4736c5667485a0a1200dc6b07ef1af91d2607
Imtiyaz <imtiyazh2003@gmail.com>
2023-03-11 13:21:10 +0530

    Initial commit

---
+++ buddies.js
@@ -0,0 +1,36 @@
+console.log("This is Imtiyaz Hussain. I'm starting the backend tutorial very soon.");
+
+// ALERT, PROMPT AND CONFIRM
+
+// alert("You're welcome to the buddies website");
+// let email = prompt("Enter your email", "abcd@gmail.com");
+// let age = prompt("Enter your age", "19");
+
+// if(age>18)
+// {
+//     console.log("You are eligible to visit the website");
+// }
+// else
+// {
+//     console.log("You are not eligible. Wait until you turn to 18");
+// }
+
+
+// DATE AND TIME
+
+// let now = new Date()
+// console.log(now);
+// setInterval(formDate, 1000);
+// function formDate()
+// {
+//     form.innerHTML = new Date();
+// }
+
+// shortcut of functions
+
+// function func12(a, b)
+// {
+//     return a+b;
+// }
+// can be written as 
+// let func1 = (a, b) => a+b;
